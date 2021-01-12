937b49db4ed02b1fcdd01e56a9c0c918a6844a52
Nick Bair <njbair@gmail.com>
2021-01-12 16:12:08 -0500

    redirect works

--- pages/support.js
+++ pages/support.js
@@ -1,2 +1,36 @@
+import React from 'react'
+import Head from 'next/head'
+import Layout from '../components/layout'
 import redirect from 'nextjs-redirect'
-export default redirect('https://airtable.com/shrsS7iLVffHzu5UX', { statusCode: 302 })
+
+class Support extends React.Component {
+  constructor(props) {
+    super(props);
+    this.listRef = React.createRef();
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    redirect('https://airtable.com/shrsS7iLVffHzu5UX', { statusCode: 302 })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Head>
+          <title>Support - Disciple Life App</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,300;1,300&family=Inter:wght@200&family=Spectral:ital,wght@0,200;1,200&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className="supportPage container my-12 mx-auto px-6">
+          <p>Redirecting you to the support form...</p>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Support
