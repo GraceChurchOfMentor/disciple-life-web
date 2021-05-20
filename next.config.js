345b64ddf66e4af58f753924e951dbfc905e3aeb
Nick Bair <njbair@gmail.com>
2021-05-20 12:17:02 -0400

    add redirect for pastors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/pastors',
+                destination: 'https://gcm.formstack.com/forms/disciple_life_app__free_gift_for_pastors',
+                permanent: false
+            },
+        ]
+    },
     target: 'serverless',
     images: {
         loader: 'imgix',
