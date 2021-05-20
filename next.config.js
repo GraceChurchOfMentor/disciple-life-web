cb8ec553e7353badf25fe9a6251d388320bac548
Nick Bair <njbair@gmail.com>
2021-05-20 12:39:57 -0400

    fix redirect

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-    async redirects() {
-        return [
-            {
-                source: '/pastors',
-                destination: 'https://gcm.formstack.com/forms/disciple_life_app__free_gift_for_pastors',
-                permanent: false
-            },
-        ]
-    },
     target: 'serverless',
     images: {
         loader: 'imgix',
---
+++ pages/pastors.js
@@ -0,0 +1,3 @@
+import redirect from 'nextjs-redirect'
+
+export default redirect('https://gcm.formstack.com/forms/disciple_life_app__free_gift_for_pastors')
